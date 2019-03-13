b2e6e3cbe7f186fcca28631bdc133a8e430ecb0a
SCHNEIDER <schneider@TOLA302007494.local>
2019-03-13 17:29:12 +0100

    up is_terminal

--- gym_jsbsim/heading_control_task.py
+++ gym_jsbsim/heading_control_task.py
@@ -264,7 +264,7 @@ class ChangeHeadingControlTask(BaseFlightTask):
         terminal_step = sim[self.steps_left] <= 0
         sim[self.nb_episodes] += 1
         #terminal_step = sim[prp.dist_travel_m]  >= 100000
-        return terminal_step or sim[prp.delta_altitude] >= 600 or sim[prp.delta_heading] >= 80
+        return terminal_step or sim[prp.altitude_sl_ft] <= 2000
     
     def _get_reward_with_heading(self, sim: Simulation, last_state: NamedTuple, action: NamedTuple, new_state: NamedTuple) -> float:
         '''

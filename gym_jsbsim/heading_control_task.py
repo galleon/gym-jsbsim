4d63548b1061e3e16ccbe5f054b387bc8dbc164c
SCHNEIDER <schneider@TOLA302007494.local>
2019-04-08 17:04:02 +0200

    up

--- gym_jsbsim/heading_control_task.py
+++ gym_jsbsim/heading_control_task.py
@@ -389,7 +389,7 @@ class TaxiControlTask(BaseFlightTask):
         self.NEW_HEADING_DEG = new_heading
         
         initial_conditions = {prp.initial_altitude_ft: self.INITIAL_ALTITUDE_FT,
-                              prp.initial_u_fps: 33.76,
+                              prp.initial_u_fps: 0,
                               prp.initial_v_fps: 0,
                               prp.initial_w_fps: 0,
                               prp.initial_p_radps: 0,

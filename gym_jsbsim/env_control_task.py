23dc71d5a8d6c3a32e7d3f8d1d5479816bd18e28
SCHNEIDER <schneider@TOLA302007494.local>
2019-05-13 16:10:27 +0200

    print

--- gym_jsbsim/env_control_task.py
+++ gym_jsbsim/env_control_task.py
@@ -559,8 +559,7 @@ class TaxiControlTask(BaseFlightTask):
         # compute shortest distance between aircarft and the path
         sim[prp.shortest_ac_to_path] = shorter_dist #shortest_path_dist # self.shortest_ac_dist(lat, lon, self.PATH[id_path_closer_point][1], self.PATH[id_path_closer_point][0], self.PATH[second_id][1], self.PATH[second_id][0])
 
-        #print(f'heading: {sim[prp.heading_deg]}, target heading: {sim[prp.target_heading_deg]}, delta_heading: {sim[prp.delta_heading]}, radius: {action.radius_circle}')
-
+        
 
         # inverse of the proportional absolute value of the minimal distance to the path
         dist_path_r = 1.0/(shorter_dist+1) #1.0/math.sqrt((0.1*sim[prp.shortest_ac_to_path]+1))
@@ -581,6 +580,8 @@ class TaxiControlTask(BaseFlightTask):
         """
         !!!!!!!! GOTO cartesian coord + check closer point and distance + check delta heading .... !!!!!!
         """
+        print(f'heading: {sim[prp.heading_deg]}, target heading: {sim[prp.target_heading_deg]}, delta_heading: {sim[prp.delta_heading]}, radius: {action.radius_circle}, reward (dist_path_r + heading_r + vel_r): {(dist_path_r,heading_r,vel_r)}')
+
         #print(f'Action: {action}, 1st, 2nd closest point id (lat,lon) = {id_path_closer_point}{(self.PATH[id_path_closer_point][1], self.PATH[id_path_closer_point][0])}, {second_id}{(self.PATH[second_id][1], self.PATH[second_id][0])}, lat,lon = {(lat,lon)}, shortest distance= {sim[prp.shortest_ac_to_path]}, a/c heading = {sim[prp.heading_deg]}, delta heading = {delta_heading}, reward heading = {heading_r}, reward dist = {dist_path_r}, reward step = {reward_nb_episode}, reward vel = {vel_r}')
         return (dist_path_r + heading_r + vel_r) / 3.0
 

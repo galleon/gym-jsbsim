8cadbfc08ec729c410bab8432a0b9f118e69c4dd
SCHNEIDER <schneider@TOLA302007494.local>
2019-05-13 15:34:44 +0200

    up

--- gym_jsbsim/env_control_task.py
+++ gym_jsbsim/env_control_task.py
@@ -522,9 +522,9 @@ class TaxiControlTask(BaseFlightTask):
                 id_path_closer_point = i
 
             # For point that are forehead (to compute the good intersection of the circle)
-            if (math.fabs(sim[prp.heading_deg] - self.calculate_initial_compass_bearing((lat,lon), (self.PATH[i][1],self.PATH[i][0])))<180):
+            if (sim[prp.heading_deg] - self.calculate_initial_compass_bearing((lat,lon), (self.PATH[i][1],self.PATH[i][0])) + 360 ) % 360 <=91:
                 dist = self.shorter_dist_point_circle(self.PATH[i][1], self.PATH[i][0], lat, lon, action.radius_circle)
-                if (dist < shorter_dist):
+                if dist > 0 and dist < shorter_dist:
                     shorter_dist = dist
                     id_path = i
             
@@ -559,7 +559,7 @@ class TaxiControlTask(BaseFlightTask):
         # compute shortest distance between aircarft and the path
         sim[prp.shortest_ac_to_path] = shorter_dist #shortest_path_dist # self.shortest_ac_dist(lat, lon, self.PATH[id_path_closer_point][1], self.PATH[id_path_closer_point][0], self.PATH[second_id][1], self.PATH[second_id][0])
 
-        print(f'heading: {sim[prp.heading_deg]}, target heading: {sim[prp.target_heading_deg]}, delta_heading: {sim[prp.delta_heading]}, radius: {action.radius_circle}')
+        #print(f'heading: {sim[prp.heading_deg]}, target heading: {sim[prp.target_heading_deg]}, delta_heading: {sim[prp.delta_heading]}, radius: {action.radius_circle}')
 
 
         # inverse of the proportional absolute value of the minimal distance to the path

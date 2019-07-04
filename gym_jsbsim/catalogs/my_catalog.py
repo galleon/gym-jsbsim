d37c90afef8672b6a78f2f942904fd3f15a8e7be
bgaudron <baptiste.gaudrondesjardins@gmail.com>
2019-07-04 16:30:02 +0200

    add property access and update method

--- gym_jsbsim/catalogs/my_catalog.py
+++ gym_jsbsim/catalogs/my_catalog.py
@@ -22,20 +22,82 @@ import gym_jsbsim.catalogs.utils as utils
 
 class MyCatalog(Property, Enum):
 
+
+    def update_delta_altitude(sim):
+        value = sim.get_property_value(JsbsimCatalog.position_h_sl_ft) - sim.get_property_value(MyCatalog.target_altitude_ft)
+        sim.set_property_value(MyCatalog.delta_altitude, value)
+
+    def update_delta_heading(sim):
+        value = utils.reduce_reflex_angle_deg(
+            sim.get_property_value(JsbsimCatalog.attitude_psi_deg) - sim.get_property_value(MyCatalog.target_heading_deg))
+        sim.set_property_value(MyCatalog.delta_heading, value)
+
+    @staticmethod
+    def update_property_incr(sim, discrete_prop, prop, incr_prop):
+        value = sim.get_property_value(discrete_prop)
+        if value == 0:
+            pass
+        else:
+            if value == 1:
+                sim.set_property_value(prop, sim.get_property_value(prop) - sim.get_property_value(incr_prop))
+            elif value == 2:
+                sim.set_property_value(prop, sim.get_property_value(prop) + sim.get_property_value(incr_prop))
+            sim.set_property_value(discrete_prop, 0)
+
+    def update_throttle_cmd_dir(sim):
+        MyCatalog.update_property_incr(sim, MyCatalog.throttle_cmd_dir, JsbsimCatalog.fcs_throttle_cmd_norm, MyCatalog.incr_throttle)
+
+
+    def update_aileron_cmd_dir(sim):
+        MyCatalog.update_property_incr(sim, MyCatalog.aileron_cmd_dir, JsbsimCatalog.fcs_aileron_cmd_norm, MyCatalog.incr_aileron)
+
+
+    def update_elevator_cmd_dir(sim):
+        MyCatalog.update_property_incr(sim, MyCatalog.elevator_cmd_dir, JsbsimCatalog.fcs_elevator_cmd_norm, MyCatalog.incr_elevator)
+
+
+    def update_rudder_cmd_dir(sim):
+        MyCatalog.update_property_incr(sim, MyCatalog.rudder_cmd_dir, JsbsimCatalog.fcs_rudder_cmd_norm, MyCatalog.incr_rudder)
+
+
+    def update_da(sim):
+        #print("sim.get_property_value(nb_step), taxi_freq_state", sim.get_property_value(nb_step), taxi_freq_state)
+        #print(taxi_freq_state)
+        if (sim.get_property_value(MyCatalog.nb_step)%sim.get_property_value(MyCatalog.taxi_freq_state)==1):
+            #start_time = time.time()
+            df = taxiPath.update_path((sim.get_property_value(JsbsimCatalog.position_lat_geod_deg), sim.get_property_value(JsbsimCatalog.position_long_gc_deg)), reader)
+            #print("--- %s seconds ---" % (time.time() - start_time))
+
+            dist = utils.shortest_ac_dist(0, 0, df[0][0].x, df[0][0].y, df[1][0].x, df[1][0].y)
+            #print("shortest_dist2", dist)
+            sim.set_property_value(MyCatalog.shortest_dist, dist)
+            #print(sim.get_property_value(shortest_dist))
+
+            for i in range(1,len(df)):
+                if (df[i][1] <= 1000000000):
+                    sim.set_property_value(MyCatalog["d"+str(i)], df[i][1])
+                    sim.set_property_value(MyCatalog["a"+str(i)], utils.reduce_reflex_angle_deg(df[i][2] - sim.get_property_value(JsbsimCatalog.attitude_psi_deg)))
+                    #sim.set_property_value(MyCatalog["a"+str(i)], df[i][2])
+                else:
+                    sim.set_property_value(MyCatalog["d"+str(i)], 1000)
+                    sim.set_property_value(MyCatalog["a"+str(i)], 0)
+        sim.set_property_value(MyCatalog.nb_step, int(sim.get_property_value(MyCatalog.nb_step))+1)
+
+
     # position and attitude
 
-    delta_heading = Property('position/delta-heading-to-target-deg', 'delta heading to target [deg]', -180, 180)
-    delta_altitude = Property('position/delta-altitude-to-target-ft', 'delta altitude to target [ft]', -40000, 40000)
+    delta_altitude = Property('position/delta-altitude-to-target-ft', 'delta altitude to target [ft]', -40000, 40000, access='R', update=update_delta_altitude)
+    delta_heading = Property('position/delta-heading-to-target-deg', 'delta heading to target [deg]', -180, 180, access='R', update=update_delta_heading)
 
     # controls command
 
-    throttle_cmd_dir = Property('fcs/throttle-cmd-dir', 'direction to move the throttle', 0, 2, Discrete)
+    throttle_cmd_dir = Property('fcs/throttle-cmd-dir', 'direction to move the throttle', 0, 2, spaces = Discrete, access = 'W',update = update_throttle_cmd_dir)
     incr_throttle = Property('fcs/incr-throttle','incrementation throttle', 0, 1)
-    aileron_cmd_dir = Property('fcs/aileron-cmd-dir', 'direction to move the aileron', 0, 2, Discrete)
+    aileron_cmd_dir = Property('fcs/aileron-cmd-dir', 'direction to move the aileron', 0, 2, spaces = Discrete, access = 'W',update = update_aileron_cmd_dir)
     incr_aileron = Property('fcs/incr-aileron', 'incrementation aileron', 0, 1)
-    elevator_cmd_dir = Property('fcs/elevator-cmd-dir', 'direction to move the elevator', 0, 2, Discrete)
+    elevator_cmd_dir = Property('fcs/elevator-cmd-dir', 'direction to move the elevator', 0, 2, spaces = Discrete, access = 'W', update = update_elevator_cmd_dir)
     incr_elevator = Property('fcs/incr-elevator', 'incrementation elevator', 0, 1)
-    rudder_cmd_dir = Property('fcs/rudder-cmd-dir', 'direction to move the rudder', 0, 2, Discrete)
+    rudder_cmd_dir = Property('fcs/rudder-cmd-dir', 'direction to move the rudder', 0, 2, spaces = Discrete, access = 'W',update = update_rudder_cmd_dir)
     incr_rudder = Property('fcs/incr-rudder', 'incrementation rudder', 0, 1)
 
     # target conditions
@@ -52,148 +114,23 @@ class MyCatalog(Property, Enum):
     turn_flight = Property('turn_flight', 'turn flight mode', 0, 1)
 
     #dist_heading_centerline_matrix = Property('dist_heading_centerline_matrix', 'dist_heading_centerline_matrix', '2D matrix with dist,angle of the next point from the aircraft to 1km (max 10 points)', [0, -45, 0, -45, 0, -45, 0, -45, 0, -45, 0, -45, 0, -45, 0, -45], [1000, 45, 1000, 45, 1000, 45, 1000, 45, 1000, 45, 1000, 45, 1000, 45, 1000, 45])
-    d1 = Property('d1', 'd1', 0, 1000) 
-    d2 = Property('d2', 'd2', 0, 1000) 
-    d3 = Property('d3', 'd3', 0, 1000) 
-    d4 = Property('d4', 'd4', 0, 1000) 
-    d5 = Property('d5', 'd5', 0, 1000) 
-    d6 = Property('d6', 'd6', 0, 1000) 
-    d7 = Property('d7', 'd7', 0, 1000) 
-    d8 = Property('d8', 'd8', 0, 1000)
-    a1 = Property('a1', 'a1', -180, 180)  
-    a2 = Property('a2', 'a2', -180, 180)  
-    a3 = Property('a3', 'a3', -180, 180)   
-    a4 = Property('a4', 'a4', -180, 180)  
-    a5 = Property('a5', 'a5', -180, 180)  
-    a6 = Property('a6', 'a6', -180, 180)  
-    a7 = Property('a7', 'a7', -180, 180)  
-    a8 = Property('a8', 'a8', -180, 180)
+    d1 = Property('d1', 'd1', 0, 1000, access = 'R', update = update_da)
+    d2 = Property('d2', 'd2', 0, 1000, access = 'R', update = update_da)
+    d3 = Property('d3', 'd3', 0, 1000, access = 'R', update = update_da)
+    d4 = Property('d4', 'd4', 0, 1000, access = 'R', update = update_da)
+    d5 = Property('d5', 'd5', 0, 1000, access = 'R', update = update_da)
+    d6 = Property('d6', 'd6', 0, 1000, access = 'R', update = update_da)
+    d7 = Property('d7', 'd7', 0, 1000, access = 'R', update = update_da)
+    d8 = Property('d8', 'd8', 0, 1000, access = 'R', update = update_da)
+    a1 = Property('a1', 'a1', -180, 180, access = 'R', update = update_da)
+    a2 = Property('a2', 'a2', -180, 180, access = 'R', update = update_da)
+    a3 = Property('a3', 'a3', -180, 180, access = 'R', update = update_da)
+    a4 = Property('a4', 'a4', -180, 180, access = 'R', update = update_da)
+    a5 = Property('a5', 'a5', -180, 180, access = 'R', update = update_da)
+    a6 = Property('a6', 'a6', -180, 180, access = 'R', update = update_da)
+    a7 = Property('a7', 'a7', -180, 180, access = 'R', update = update_da)
+    a8 = Property('a8', 'a8', -180, 180, access = 'R', update = update_da)
 
-    shortest_dist = Property('shortest_dist', 'shortest distance between aircraft and path [m]', 0.0, 1000.0)
+    shortest_dist = Property('shortest_dist', 'shortest distance between aircraft and path [m]', 0.0, 1000.0, access = 'R', update = update_da)
     taxi_freq_state = Property('taxi-freq-state','frequence to update taxi state',0)
-    nb_step = Property('nb_step', 'shortest distance between aircraft and path [m]')
-
-
-    # functions updating custom properties
-
-    @classmethod
-    def update_delta_altitude(cls, sim):
-        value = sim.get_property_value(JsbsimCatalog.position_h_sl_ft) - sim.get_property_value(cls.target_altitude_ft)
-        sim.set_property_value(cls.delta_altitude, value)
-
-    @classmethod
-    def update_delta_heading(cls, sim):
-        value = utils.reduce_reflex_angle_deg(sim.get_property_value(JsbsimCatalog.attitude_psi_deg) - sim.get_property_value(cls.target_heading_deg))
-        sim.set_property_value(cls.delta_heading, value)
-
-    @staticmethod
-    def update_property_incr(sim, discrete_prop, prop, incr_prop):
-        value = sim.get_property_value(discrete_prop)
-        if value == 0:
-            pass
-        else :
-            if value == 1:
-                sim.set_property_value(prop, sim.get_property_value(prop) - sim.get_property_value(incr_prop))
-            elif value == 2:
-                sim.set_property_value(prop, sim.get_property_value(prop) + sim.get_property_value(incr_prop))
-            sim.set_property_value(discrete_prop, 0)
-
-    @classmethod
-    def update_throttle_cmd_dir(cls, sim):
-        cls.update_property_incr(sim, cls.throttle_cmd_dir, JsbsimCatalog.fcs_throttle_cmd_norm, MyCatalog.incr_throttle)
-
-    @classmethod
-    def update_aileron_cmd_dir(cls, sim):
-        cls.update_property_incr(sim, cls.aileron_cmd_dir, JsbsimCatalog.fcs_aileron_cmd_norm, MyCatalog.incr_aileron)
-
-    @classmethod
-    def update_elevator_cmd_dir(cls, sim):
-        cls.update_property_incr(sim, cls.elevator_cmd_dir, JsbsimCatalog.fcs_elevator_cmd_norm, MyCatalog.incr_elevator)
-
-    @classmethod
-    def update_rudder_cmd_dir(cls, sim):
-        cls.update_property_incr(sim, cls.rudder_cmd_dir, JsbsimCatalog.fcs_rudder_cmd_norm, MyCatalog.incr_rudder)
-
-
-    @classmethod
-    def update_da(cls, sim):
-        #print("sim.get_property_value(nb_step), taxi_freq_state", sim.get_property_value(nb_step), taxi_freq_state)
-        #print(taxi_freq_state)
-        if (sim.get_property_value(cls.nb_step)%taxi_freq_state==1):
-            #start_time = time.time()
-            df = taxiPath.update_path((sim.get_property_value(JsbsimCatalog.position_lat_geod_deg), sim.get_property_value(JsbsimCatalog.position_long_gc_deg)), reader)
-            #print("--- %s seconds ---" % (time.time() - start_time))
-            
-            dist = utils.shortest_ac_dist(0, 0, df[0][0].x, df[0][0].y, df[1][0].x, df[1][0].y)
-            #print("shortest_dist2", dist)
-            sim.set_property_value(cls.shortest_dist, dist)
-            #print(sim.get_property_value(shortest_dist))
-
-            for i in range(1,len(df)):
-                if (df[i][1] <= 1000000000):
-                    sim.set_property_value(cls.get_var_by_name("d"+str(i)), df[i][1])
-                    sim.set_property_value(cls.get_var_by_name("a"+str(i)), utils.reduce_reflex_angle_deg(df[i][2] - sim.get_property_value(JsbsimCatalog.attitude_psi_deg)))
-                    #sim.set_property_value(cls.get_var_by_name("a"+str(i)), df[i][2])
-                else:
-                    sim.set_property_value(cls.get_var_by_name("d"+str(i)), 1000)
-                    sim.set_property_value(cls.get_var_by_name("a"+str(i)), 0)
-        sim.set_property_value(cls.nb_step, int(sim.get_property_value(cls.nb_step))+1)
-
-    @classmethod
-    def update_custom_properties(cls,sim,prop):
-        update_custom_properties = {cls.delta_altitude : cls.update_delta_altitude,
-
-                                    cls.delta_heading: cls.update_delta_heading,
-
-                                    cls.throttle_cmd_dir: cls.update_throttle_cmd_dir,
-
-                                    cls.aileron_cmd_dir: cls.update_aileron_cmd_dir,
-
-                                    cls.elevator_cmd_dir : cls.update_elevator_cmd_dir,
-
-                                    cls.rudder_cmd_dir : cls.update_rudder_cmd_dir#,
-
-                                    #cls.d1 : cls.update_da
-                                }
-        if prop in update_custom_properties:
-            update_custom_properties[prop](sim)
-
-            
-    @classmethod
-    def init_custom_properties(cls,sim,prop):
-        init_custom_properties = { cls.taxi_freq_state : 30,
-                                   
-                                   cls.incr_throttle: 0.05,
-
-                                   cls.incr_aileron : 0.05,
-
-                                   cls.incr_elevator : 0.05,
-
-                                   cls.incr_rudder : 0.05
-        }
-
-        if prop in init_custom_properties:
-            sim.set_property_value(prop,init_custom_properties[prop])
-
-    @classmethod
-    def get_var_by_name(cls, string):
-        dict_da = {
-        "d1": cls.d1,
-        "d2": cls.d2,
-        "d3": cls.d3,
-        "d4": cls.d4,
-        "d5": cls.d5,
-        "d6": cls.d6,
-        "d7": cls.d7,
-        "d8": cls.d8,
-        "a1": cls.a1,
-        "a2": cls.a2,
-        "a3": cls.a3,
-        "a4": cls.a4,
-        "a5": cls.a5,
-        "a6": cls.a6,
-        "a7": cls.a7,
-        "a8": cls.a8
-        }
-        return dict_da[string]
-
+    nb_step = Property('nb_step', 'shortest distance between aircraft and path [m]', access = 'R',update = update_da)
--- gym_jsbsim/catalogs/property.py
+++ gym_jsbsim/catalogs/property.py
@@ -2,4 +2,4 @@ from collections import namedtuple
 from gym.spaces import Box, Discrete
 
 
-Property = namedtuple('Property', ['name_jsbsim', 'description', 'min', 'max', 'spaces'], defaults=(None, None, float('-inf'), float('+inf'), Box))
+Property = namedtuple('Property', ['name_jsbsim', 'description', 'min', 'max', 'access', 'spaces', 'update'], defaults=(None, None, float('-inf'), float('+inf'),'RW', Box, None))

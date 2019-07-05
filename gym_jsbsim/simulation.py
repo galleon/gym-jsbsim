f5c45f31534f454f1bf978406a5dfc74af3b83ba
SCHNEIDER <schneider@TOLA302007494.local>
2019-07-05 15:43:03 +0200

    rename sim.get_full_state() to sim.get_state() and sim.get_init_conditions_from_state to sim.set_state

--- gym_jsbsim/simulation.py
+++ gym_jsbsim/simulation.py
@@ -212,7 +212,7 @@ class Simulation:
             MyCatalog.update_custom_properties(self,prop)
 
 
-    def get_full_state(self):
+    def get_state(self):
         full_jsbsim_state = self.jsbsim_exec.get_property_catalog('')
         full_state = dict()
         for prop_jsbsim,value in full_jsbsim_state.items():
@@ -228,7 +228,7 @@ class Simulation:
                 pass
         return full_state
 
-    def get_init_conditions_from_state(self,state):
+    def set_state(self,state):
         init_conditions = {}
 
         state_to_ic = {JsbsimCatalog.position_lat_gc_deg: JsbsimCatalog.ic_lat_gc_deg,

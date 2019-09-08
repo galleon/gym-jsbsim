3f6e36a054c0ec09cff33bf695aaebb744037653
jyotsnab <jyotsna1710@gmail.com>
2019-09-08 14:36:30 +0200

    taxi utils refactored

--- gym_jsbsim/catalogs/my_catalog.py
+++ gym_jsbsim/catalogs/my_catalog.py
@@ -71,7 +71,7 @@ class MyCatalog(Property, Enum):
         #print(taxi_freq_state)
         #if (sim.get_property_value(MyCatalog.nb_step)%sim.get_property_value(MyCatalog.taxi_freq_state)==1):
         #start_time = time.time()
-        df = taxiPath.update_path((sim.get_property_value(JsbsimCatalog.position_long_gc_deg), sim.get_property_value(JsbsimCatalog.position_lat_geod_deg)),sim.get_property_value(JsbsimCatalog.attitude_psi_deg))
+        df = taxiPath.update_path((sim.get_property_value(JsbsimCatalog.position_long_gc_deg), sim.get_property_value(JsbsimCatalog.position_lat_geod_deg)))
         #print("--- %s seconds ---",(time.time() - start_time))
 
         dist = taxiPath.shortest_dist

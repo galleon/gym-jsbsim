a75c22d4adc14b7f72aa0a21650326f1567cac6b
GAUDRON DESJARDINS <GAUDRON_B@eu.airbus.corp>
2019-10-21 11:54:58 +0200

    fix linux deployment

--- build_wheels.sh
+++ build_wheels.sh
@@ -9,6 +9,6 @@ done
 
 # Bundle external shared libraries into the wheels
 for whl in dist/*.whl; do
-    auditwheel repair "$whl" --plat $PLAT -w dist/
+    auditwheel repair "$whl" --plat manylinux2010_x86_64 -w dist/
 done
 

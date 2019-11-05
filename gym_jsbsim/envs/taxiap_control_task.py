e57f5ff5049950f46194d569912f7347f9347581
SCHNEIDER <schneider@TOLA302007494.local>
2019-11-05 17:09:14 +0100

    clean and up envs

--- gym_jsbsim/envs/taxiap_control_task.py
+++ gym_jsbsim/envs/taxiap_control_task.py
@@ -14,10 +14,10 @@ class TaxiapControlTask(Task):
     action_var = [c.fcs_steer_cmd_norm]
 
     k2f = 1.68781 # Knot to feet
-    # INIT_AC_LON = 1.369889125000043  # loop
-    # INIT_AC_LAT = 43.625578879000045 # loop
-    INIT_AC_LON = 1.37211666700005708 # attol
-    INIT_AC_LAT = 43.6189638890000424 # attol
+    INIT_AC_LON = 1.369889125000043  # loop
+    INIT_AC_LAT = 43.625578879000045 # loop
+    # INIT_AC_LON = 1.37211666700005708 # attol
+    # INIT_AC_LAT = 43.6189638890000424 # attol
     INIT_AC_HEADING = 323
     INITIAL_ALTITUDE_FT = 8.42
     INITIAL_VELOCITY_U = 7.0 * k2f # 20 knots/sec

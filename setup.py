a6261c027046d0e49808a82b27a9c5b6e68615a7
bgaudron <51315116+bgaudron@users.noreply.github.com>
2019-11-06 17:28:25 +0100

    correct version format

--- setup.py
+++ setup.py
@@ -64,5 +64,5 @@ setup(
                            language='c++')],
     package_data={'gym_jsbsim': ['jsbsim/aircraft/*/*.xml','jsbsim/systems/*.xml','jsbsim/engine/*.xml']},
     install_requires=['cython>=0.25','gym>=0.12.5','shapely','geographiclib'],
-    use_scm_version=True,
+    use_scm_version={"version_scheme": lambda v: v.format_with('{tag}' if not v.distance>0 else '{tag}.dev{distance}'), "local_scheme": lambda v: ""},
     setup_requires=['cython>=0.25', 'setuptools_scm'])

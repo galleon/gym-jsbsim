0a95db24895f2651364d7004a41e983e6f92ba11
Guillaume Alleon <guillaume.alleon@airbus.com>
2019-11-19 06:20:30 +0100

    Removed tests for state serialization

--- gym_jsbsim/tests/test_simulation.py
+++ gym_jsbsim/tests/test_simulation.py
@@ -28,6 +28,7 @@ class TestSimulation(unittest.TestCase):
     def tearDown(self):
         self.env.close()
 
+"""
     def test_get_state_set_state_no_action(self):
 
         self.env.reset()
@@ -64,7 +65,6 @@ class TestSimulation(unittest.TestCase):
             else:
                 error = math.fabs(p2 - p1)/max(math.fabs(p1), math.fabs(p2))
             self.assertLess(error, self.error_max, 'The two simulations have diverged')
-
 
     def test_get_state_set_state_constant_action(self):
         constant_action = [1, 1, 1, 1]
@@ -103,7 +103,6 @@ class TestSimulation(unittest.TestCase):
             else:
                 error = math.fabs(p2 - p1) / max(math.fabs(p1), math.fabs(p2))
             self.assertLess(error, self.error_max, 'The two simulations have diverged')
-
 
     def test_get_state_set_state_oscillating_altitude(self):
         # taking actions only on the elevator command
@@ -154,3 +153,4 @@ class TestSimulation(unittest.TestCase):
             else:
                 error = math.fabs(p2 - p1)/max(math.fabs(p1), math.fabs(p2))
             self.assertLess(error, self.error_max, 'The two simulations have diverged')
+"""

4c23b3e836aa3c4fcd390e0ae2d9967832a75c5e
galleon <guillaume.alleon@gmail.com>
2020-10-23 06:58:39 +0000

    Removed directory containing build scripts

--- build_tools/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [[ "$TRAVIS_OS_NAME" != "linux" ]]; then
-  python3 setup.py bdist_wheel || python setup.py bdist_wheel
-  twine upload --skip-existing --username "galleon" --password "$PASSWORD" dist/*.whl
-else
-  twine upload --skip-existing --username "galleon" --password "$PASSWORD" dist/*manylinux*.whl
-fi
